d626abe00fbac5e9a72f7fa54fb83fe0a8d3d906
dung-vt <tiendungvan89@gmail.com>
2020-01-24 08:28:00 +0900

    add ex

---
+++ ValidateEx/ValidateEx/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using ValidateEx.Models;
+
+namespace ValidateEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() => View("MakeBooking", new Appointment { Date = DateTime.Now });
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt) {
+            if (string.IsNullOrEmpty(appt.ClientName))
+            {
+                ModelState.AddModelError(nameof(appt.ClientName),
+                        "Please enter your name");
+            }
+
+            if (ModelState.GetValidationState("Date") == ModelValidationState.Valid && DateTime.Now > appt.Date)
+            {
+                ModelState.AddModelError(nameof(appt.Date), "Please enter a date in the future");
+            }
+
+            //if (!appt.TermsAccepted)
+            //{
+            //    ModelState.AddModelError(nameof(appt.TermsAccepted), "You must accept the terms");
+            //}
+
+            if (ModelState.GetValidationState(nameof(appt.Date)) == ModelValidationState.Valid
+                && ModelState.GetValidationState(nameof(appt.ClientName)) == ModelValidationState.Valid
+                && appt.ClientName.Equals("Joe", StringComparison.OrdinalIgnoreCase)
+                && appt.Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError("",
+                "Joe cannot book appointments on Mondays");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("Completed", appt);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ValidateEx/ValidateEx/Models/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ValidateEx.Attributes;
+
+namespace ValidateEx.Models
+{
+    public class Appointment
+    {
+        [Required]
+        [Display(Name = "name")]
+        public string ClientName { get; set; }
+
+        [UIHint("Date")]
+        [Required(ErrorMessage = "Please enter a date")]
+        public DateTime Date { get; set; }
+
+        [MustBeTrue(ErrorMessage = "You must accept the terms")]
+        public bool TermsAccepted { get; set; }
+    }
+}
